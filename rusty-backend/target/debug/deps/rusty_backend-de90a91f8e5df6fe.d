f3b423988003f4120d43cde236ca67863b993182
matdexir <matthieudesir21@gmail.com>
2022-06-21 01:26:51 +0800

    Total rewrite in rust for no reason

--- rusty-backend/target/debug/deps/rusty_backend-de90a91f8e5df6fe.d
+++
@@ -1,5 +0,0 @@
-/home/matdexir/Files/Projects/evr-rust/rusty-backend/target/debug/deps/rusty_backend-de90a91f8e5df6fe.rmeta: src/lib.rs
-
-/home/matdexir/Files/Projects/evr-rust/rusty-backend/target/debug/deps/rusty_backend-de90a91f8e5df6fe.d: src/lib.rs
-
-src/lib.rs:
--- rusty-backend/target/debug/deps/rusty_backend-e9b04992322d1b7d.d
+++
@@ -1,5 +0,0 @@
-/home/matdexir/Files/Projects/evr-rust/rusty-backend/target/debug/deps/rusty_backend-e9b04992322d1b7d.rmeta: src/lib.rs
-
-/home/matdexir/Files/Projects/evr-rust/rusty-backend/target/debug/deps/rusty_backend-e9b04992322d1b7d.d: src/lib.rs
-
-src/lib.rs:
